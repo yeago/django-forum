a03506e95a1623a54642177193bbc6493d0bdbf8
yeago <subsume@gmail.com>
2011-07-23 16:16:26 -0400

    removed another lib

--- forum/admin.py
+++ forum/admin.py
@@ -3,28 +3,6 @@ from django.db import models
 from django.contrib.admin.widgets import FilteredSelectMultiple
 from forum.models import Forum, Thread
 
-def convert_thread(modeladmin,request,qs):
-	from django.contrib.contenttypes.models import ContentType
-	comment_model = comments.get_model()
-	from stack.models import Question
-	old_ct = ContentType.objects.get_for_model(Thread)
-
-	from django.contrib import comments
-
-	comment_model = comments.get_model()
-
-	for thread in qs:
-		first_comment = comment_model.objects.filter(content_type=old_ct,object_pk=thread.pk).order_by('submit_date')[0]
-
-		question = Question.objects.create(title=thread.title,comment=first_comment)
-		ct = ContentType.objects.get_for_model(question)
-		comment_model.objects.filter(content_type=old_ct,object_pk=thread.pk).update(content_type=ct,object_pk=question.pk)
-
-		from former_url.models import FormerURL
-		FormerURL.objects.create(current=question.get_absolute_url(),former=thread.get_absolute_url())
-
-		thread.delete()
-
 class ForumAdmin(admin.ModelAdmin):
     list_display = ('title', '_parents_repr')
     ordering = ['ordering', 'parent', 'title']
