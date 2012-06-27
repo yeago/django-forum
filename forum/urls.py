03bb35963187377ecfc471ed5795672dfc881a54
yeago <subsume@gmail.com>
2012-06-26 18:16:17 -0700

    forgot urls

--- forum/urls.py
+++ forum/urls.py
@@ -14,11 +14,6 @@ from forum.models import Forum
 from forum.feeds import RssForumFeed, AtomForumFeed
 from forum.sitemap import ForumSitemap, ThreadSitemap, PostSitemap
 
-feed_dict = {
-    'rss' : RssForumFeed,
-    'atom': AtomForumFeed
-}
-
 sitemap_dict = {
     'forums': ForumSitemap,
     'threads': ThreadSitemap,
@@ -27,9 +22,8 @@ sitemap_dict = {
 
 urlpatterns = patterns('',
     url(r'^$', 'forum.views.forums_list', name='forum_list'),
-    
-    url(r'^(?P<url>(rss|atom).*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feed_dict}),
-
+    (r'^(?P<url>(rss).*)/$', RssForumFeed()),
+    (r'^(?P<url>(atom).*)/$', AtomForumFeed()),
     url(r'^(?P<slug>[-\w]+)/$', 'forum.views.forum', name='forum_thread_list'),
     url(r'^(?P<forum>[-\w]+)/new/$', 'forum.views.newthread', name='forum_new_thread'),
 
