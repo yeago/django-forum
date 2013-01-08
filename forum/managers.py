85741a47532c5f525229f9b86d10b666d97c5b3d
yeago <subsume@gmail.com>
2013-01-07 16:42:09 -0800

    unfetter

--- forum/managers.py
+++ forum/managers.py
@@ -3,6 +3,7 @@ from django.db.models import Q
 
 class ForumManager(models.Manager):
     def for_user(self, u):
+        return self.all()
         if u and u.is_authenticated():
             public = Q(allowed_users__isnull=True)
             user = Q(allowed_users=u)
@@ -10,6 +11,7 @@ class ForumManager(models.Manager):
         return self.filter(allowed_users__isnull=True)
 
     def for_groups(self, groups):
+        return self.all()
         if groups:
             public = Q(groups__isnull=True)
             user_groups = Q(groups__in=groups)
@@ -17,4 +19,5 @@ class ForumManager(models.Manager):
         return self.filter(groups__isnull=True)
     
     def has_access(self, forum, user):
+        return True
         return forum in self.for_user(user)
