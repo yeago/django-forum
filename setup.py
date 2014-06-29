1f892142d8b2c9676a4266d75d94af34fb060f06
yeago <subsume@gmail.com>
2014-06-29 08:28:22 +0200

    x

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-shitty-forum',
-    version="0.2",
+    version="0.3",
     author='Steve Yeago',
     author_email='subsume@gmail.com',
     description='Forum in Django. Don\'t use it, it sucks.',
