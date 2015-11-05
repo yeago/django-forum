7bea30581738f77d39ee36689448766aae1f0062
=yeago <yeago999@gmail.com>
2015-11-05 03:15:20 +0000

    don't ref this it never gets ran

--- forum/migrations/0001_initial.py
+++ forum/migrations/0001_initial.py
@@ -10,7 +10,6 @@ class Migration(migrations.Migration):
     dependencies = [
         ('sites', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('comments_app', '0002_auto_20151104_2141'),
     ]
 
     operations = [
