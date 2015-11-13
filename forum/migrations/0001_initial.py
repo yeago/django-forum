829787ed91c4eaabded9d6745605f538b391ed8e
Ignacio Vaggione <ivaggione@gmail.com>
2015-11-12 21:58:20 -0300

    add dependency

--- forum/migrations/0001_initial.py
+++ forum/migrations/0001_initial.py
@@ -10,6 +10,7 @@ class Migration(migrations.Migration):
     dependencies = [
         ('sites', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('comments_app', '0001_initial'),
     ]
 
     operations = [
